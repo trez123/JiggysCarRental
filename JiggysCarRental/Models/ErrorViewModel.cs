124843f00d50f6d865abc2aaaffdbf2d8527efbf
Silas Coley <silascoley3@gmail.com>
2023-08-08 13:03:05 -0400

    initial

---
+++ JiggysCarRental/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JiggysCarRental.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
