0699cc12f565762ce427e08758e17dc50046e25e
Silas Coley <silascoley3@gmail.com>
2023-08-09 02:22:43 -0400

    front page changes

--- JiggysCarRental/Controllers/HomeController.cs
+++ JiggysCarRental/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using JiggysCarRental.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace JiggysCarRental.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly CarDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, CarDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        return _context.Vehicles != null ?
+                         View(await _context.Vehicles.ToListAsync()) :
+                         Problem("Entity set 'CarDbContext.Vehicles'  is null.");
     }
 
     public IActionResult Privacy()
